a7942cf6d30f70856c4b210ee9765a8bfd31cc91
joemt19 <joembanga19@gmail.com>
2025-12-23 01:30:14 +0100

    Adding bases for a darkmode

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -163,18 +163,7 @@
         </div>
     </section>
 
-    <footer class="bg-white border-t border-gray-200">
-        <div class="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
-            <div class="mt-8 md:order-1 md:mt-0">
-                <p class="text-center text-xs leading-5 text-gray-500">&copy; {{ date('Y') }} IrreguLearn, Kinshasa.
-                    Tous droits réservés.</p>
-            </div>
-            <div class="flex justify-center space-x-6 md:order-2">
-                <a href="#" class="text-gray-400 hover:text-gray-500">Instagram</a>
-                <a href="#" class="text-gray-400 hover:text-gray-500">Twitter</a>
-            </div>
-        </div>
-    </footer>
+    @include('layouts.footer')
 
 </body>
 
