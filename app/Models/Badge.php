3cf60d562112be841cfc9f9e72ce948e7ba9e184
joemt19 <joembanga19@gmail.com>
2025-12-27 01:15:53 +0100

    LIttle changes

--- app/Models/Badge.php
+++ app/Models/Badge.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Badge extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class)->withTimestamps();
+    }
 }
